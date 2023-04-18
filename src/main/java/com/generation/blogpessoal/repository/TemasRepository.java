c4ef4cb5633d499026eb3983426380f882f81cd3
Letícia Queiroga <letqueirogaa@outlook.com>
2023-04-17 21:17:01 -0300

    [Feature Leticia - Task : Ajustes | 17.04.2023]

--- src/main/java/com/generation/blogpessoal/repository/TemasRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/TemasRepository.java
@@ -1,9 +1,12 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Temas;
 
 public interface TemasRepository extends JpaRepository<Temas, Long>{
-	
+	public List<Temas> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 }
